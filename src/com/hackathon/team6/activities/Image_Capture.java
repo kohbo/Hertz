b0326e94f34d236ba9263043770af4a0aa2dc473
Juan Menendez <kohbosan@gmail.com>
2015-01-25 05:58:38 -0500

    j

--- src/com/hackathon/team6/activities/Image_Capture.java
+++ src/com/hackathon/team6/activities/Image_Capture.java
@@ -1,7 +1,6 @@
 package com.hackathon.team6.activities;
 
 import android.app.ActionBar;
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
